fc7451c04afff5b598c4897c69b2c0a372fad63a
Dennaja <dendradetama2@gmail.com>
2024-12-05 00:09:06 +0800

    perubahan dikit

--- js/script.js
+++ js/script.js
@@ -9,12 +9,22 @@ scrollButton.addEventListener('click', () => {
 });
 
 
-const AbousButton = document.getElementById('AboutB');
+const AboutButton = document.getElementById('AboutB');
 const AboutSection = document.getElementById('AboutS');
 
 // Tambahkan event listener pada tombol
-AbousButton.addEventListener('click', () => {
+AboutButton.addEventListener('click', () => {
     AboutSection.scrollIntoView({
+    behavior: 'smooth' // Animasi scroll halus
+    });
+});
+
+const ToolsButton = document.getElementById('ToolsB');
+const ToolsSection = document.getElementById('ToolsS');
+
+// Tambahkan event listener pada tombol
+ToolsButton.addEventListener('click', () => {
+    ToolsSection.scrollIntoView({
     behavior: 'smooth' // Animasi scroll halus
     });
 });
